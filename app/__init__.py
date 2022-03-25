2651d209b1e6f314a610f8d42da4cc3f51264cf3
Bryan Marshall <professor@bryanmarshall.com>
2022-03-25 11:38:27 -0400

    database update

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 #database configuration
+# each part of the connection is listed. The typical database on AWS is named sys.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@aws_server_host/database'
 
 # create the database
