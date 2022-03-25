fcf6f8bb720e9313031fc0b46a306e8cc4d227a1
Bryan Marshall <professor@bryanmarshall.com>
2022-03-24 21:45:58 -0400

    login stuff

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,29 @@
-from flask import render_template
-from app import app
+from flask import render_template, url_for, redirect
+from flask_login import login_user, logout_user, current_user, login_required, LoginManager
+from app import app, db
+from app.models import User, Team, Practice
 
+
+#### LOGIN STUFF NEEDED
+##### USER MANAGEMENT ##### FOUND: https://www.youtube.com/watch?v=2dEM-s3mRLE
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# This method is used to as part of the flask_login code.
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def index():
     return  render_template('index.html')
+
+
+
+### LOGIN LOGOUT STUFF ####
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
